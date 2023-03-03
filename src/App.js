449522683e0bdfd85fb6c4708f6f1a23cf6809c7
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-03-03 22:33:43 +0000

    react bootstram react-router installed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <p class="display-1">Mi aplicacion</p>
+    </div>
+  );
+}
+
+export default App;
