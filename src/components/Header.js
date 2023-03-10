40cace721d954dc42a5c181918546c73d68a8d87
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-03-10 00:48:11 +0000

    added some style to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React from "react";
+import myself from "../assets/images/me.JPG"
 
 function Header() {
   return (
   <div class="container-fluid">
     <nav class="navbar navbar-expand-lg navbar-dark">
-  <div class="container">
-    <a class="navbar-brand" href="#"><span class="h3" fw-bold>Jose Alonso</span></a>
+  <div class="container flex-lg-column">
+    <a class="navbar-brand mb-lg-4 mx-lg-auto" href="#Home"><span class="h3 fw-bold d-block d-lg-none">Jose Alonso</span>
+    <img src={myself} class="d-none d-lg-block rounded-circle" alt="me"></img>
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav ms-auto">
+      <div class="navbar-nav ms-auto flex-lg-column text-lg-center">
         <a class="nav-link active" aria-current="page" href="/">Home</a>
         <a class="nav-link" href="/projects">Projects</a>
         <a class="nav-link" href="/contact">Contact</a>
