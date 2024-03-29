17fddfb48293acb356d85e761e3ac139274b3cb3
turquele <jose.m.alonso.g@gmail.com>
2024-03-29 14:51:48 +0000

    Changed wording in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,10 +8,10 @@ function Home() {
         <div className="container">
           <div className="row">
             <div className="col-lg-8">
-              <h1 className="display-4 fw-bold">I'M A <span className="text-brand">UI/UX DESIGNER</span></h1>
-              <p className="lead mt-2 mb-4">I am Jose Alonso, And I develope high performing websites</p>
+              <h1 className="display-4 fw-bold">UI/UX <span className="text-brand">DESIGNER</span></h1>
+              <p className="lead mt-2 mb-4">I am Jose Alonso, I develope beautiful high performing websites</p>
               <a href="/projects" className="btn btn-brand me-3">Explore My Work</a>
-              <a href="#" className="link-custom">Call: +44 (0) 79072 67138</a>
+              <a href="#" className="link-custom">Phone: +44 (0) 79072 67138</a>
             </div>
           </div>
         </div>
